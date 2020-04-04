a21fb22a0a64dd2b3357f4e1049ec307a43e9a63
Oliver Wright <meiamsome@meiamso.me>
2020-04-04 01:16:22 +0100

    Cleanup (#8)
     - Removes some ts-ignores and tightens typescript restrictions generally
     - Should now support all GraphQL schemas, even if they have weird field names like `_configureAbstractProperty` or `resolveField`

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,22 @@
-export const lazyProperty = <T>(object: any, property: string | number, creator: () => T) => {
+// Gets the union type of all keys of T
+type ValueOf<T> = T[keyof T]
+
+export const lazyProperty = <
+  Result,
+  // Checks the index of Target extends Result | ValueOf<Target>, i.e. that Result is valid to be added to the object based on index type
+  Target extends { [index: string]: Result | ValueOf<Target> }
+>(
+    target: Target,
+    property: string,
+    instantiator: () => Result,
+  ) => {
   let init = false
-  let data: T
-  Object.defineProperty(object, property, {
+  let data: Result
+  Object.defineProperty(target, property, {
     get: () => {
       if (!init) {
         init = true
-        data = creator()
+        data = instantiator()
       }
       return data
     },
